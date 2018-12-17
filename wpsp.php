020778cd741253748db5dcdcfb16c3e655f26c88
Andrew Zito <zitoa@lafayette.edu>
2018-12-17 11:47:40 -0500

    Base (wp scaffold)

---
+++ wpsp.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Wpsp
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     wpsp
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wpsp
+ */
+
+// Your code starts here.
