d4aedd767b7480b5af033e33cc6d9668de7088a8
Andrew Zito <zitoa@lafayette.edu>
2019-02-18 15:21:37 -0500

    Default user data values

--- src/classes/User.php
+++ src/classes/User.php
@@ -8,15 +8,17 @@ class User {
 
     protected $id;
     protected $login;
+    protected $firstname;
+    protected $lastname;
     protected $roles;
 
     public function __construct( $userdata ) {
         $userdata        = (array) $userdata;
-        $this->id        = $userdata[ 'ID' ];
-        $this->login     = $userdata[ 'user_login' ];
-        $this->firstname = $userdata[ 'first_name' ];
-        $this->lastname  = $userdata[ 'last_name' ];
-        $this->email     = $userdata[ 'user_email' ];
+        $this->id        = isset( $userdata[ 'ID' ] )         ? $userdata[ 'ID' ]         : '';
+        $this->login     = isset( $userdata[ 'user_login' ] ) ? $userdata[ 'user_login' ] : '';
+        $this->firstname = isset( $userdata[ 'first_name' ] ) ? $userdata[ 'first_name' ] : '';
+        $this->lastname  = isset( $userdata[ 'last_name' ] )  ? $userdata[ 'last_name' ]  : '';
+        $this->email     = isset( $userdata[ 'user_email' ] ) ? $userdata[ 'user_email' ] : '';
     }
 
     public function getFullname() {
